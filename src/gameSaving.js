0fa3d655541a4c9f00aa135d7f38c6e6f2c7f004
Tatiana Nikonova <taniushka_2007@mail.ru>
2022-01-05 20:27:48 +0300

    First

---
+++ src/gameSaving.js
@@ -0,0 +1,8 @@
+export default class GameSaving {
+  constructor(gameData) {
+    const { id, created, userInfo } = gameData;
+    this.id = id;
+    this.created = created;
+    this.userInfo = userInfo;
+  }
+}
